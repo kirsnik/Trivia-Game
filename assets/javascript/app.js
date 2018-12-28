52015cf069e71d901b9b7b22d68d80c4f805c9bd
Rene.Mendes <gwingster@gmail.com>
2018-12-27 18:11:05 -0800

    added clickevent

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,91 +8,74 @@ $.ajax({ /*   */
 }).then(function (response) { /*   */
     console.log('response', response); /*   */
 
-    // console.log(response.results[0].question, response.results[0].incorrect_answers + "," + response.results[0].correct_answer,"this is questions")
-
-    var questionGame = response.results[0].question;
-
-    for (let i = 0; i < response.results.length; i++){
-        console.log('results', response.results[i]);
-        console.log('i', );
-        
-        var qc = $("<p>").text(response.results[i].question);
-         $("#quiz").append(qc);
-
-        for( let j = 0; j < response.results[i].incorrect_answers.length; j++){
-            console.log("asdf")
-            
-         var radioButton = $('<input type="radio" value=' + response.results[i].incorrect_answers[j]+'> <label>'+  response.results[i].incorrect_answers[j] + '</label>');
-         $("#quiz").append(radioButton);
-        }       
 
 
 
+    function scoreboard() {
+
+
+        var correctPoint = 0;
+        var incorrectPoint = 0;
+        var unasweredPoint = 0;
+        var timer = 30;
 
     }
-  
-    // console.log('questionGame', questionGame);
-    // var qg = $("<p>").text(questionGame);
-    // $("#quiz").prepend(qg);
+
+    for (let i = 0; i < response.results.length; i++) {
+        var qc = $("<p>").text(response.results[i].question);
+        $("#quiz").append(qc);
+
+        var incorrectAns = response.results[i].incorrect_answers;
+        var correctAns = response.results[i].correct_answer;
+        var randomArr = incorrectAns.concat(correctAns);
+        var answerArr = randomChoices(randomArr);
+
+        console.log('answerArr', answerArr);
+        for (let j = 0; j < answerArr.length; j++) {
+            // console.log("asdf")
+
+
+            var radioButton = $('<button value=' + randomChoices(answerArr[j]) + '> ' + randomChoices(answerArr[j]) + '</button>');
+            $("#quiz").append(radioButton);
+
+           
+        }
+    }
+
+    $( "button" ).click( function(){
+        console.log(event.currentTarget.innerHTML);
+
+        var number = parseInt($('#youranswerdiv').text());
+          number+=1;
+          $('#youranswerdiv').text(number);
+
+      });
+
+
+    
+
 
     function randomChoices(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
-            while (0 !== currentIndex) {
-    
-                // Pick a remaining element...
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex -= 1;
-    
-                // And swap it with the current element.
-                temporaryValue = array[currentIndex];
-                array[currentIndex] = array[randomIndex];
-                array[randomIndex] = temporaryValue;
-            }
-            return array;
+        var currentIndex = array.length,
+            temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
     }
 
 
-    var questionChoice = [" "];
-    questionChoice.push([response.results[0].correct_answer] + [response.results[0].incorrect_answers]);
-    console.log('combo1', response.results[0].correct_answer);
-    console.log('combo2', response.results[0].incorrect_answers);
-    console.log('first var', questionChoice);
-    questionChoice = randomChoices(questionChoice);
-    console.log('questionChoice random', questionChoice);
-    $("#quiz").append(qc);
-
-    // var correctAns = response.results[0].correct_answer;
-    // var incorrectAns = response.results[0].incorrect_answers;
-    // var gameChoices = correctAns +","+ incorrectAns;
-    // console.log('gameChoices', gameChoices);
 
 
 
-
-
-
-
-
-    // var ourQuestions = respone.questions
-
-    // console.log(ourQuestions[0].question, " number 2");    /*   */
-
-    // var questionDiv = $("<div>");    /*   */
-    // var p = $("<p>").text("QUESTIONS: " + ourQuestions[0].question);    /*   */
-
-    // $(questionDiv).append(p);    /*   */
-    // $("#quiz").append(questionDiv);    /*   */
-
-    // var correct_answer = response.results[0].correct_answer;    /*   */
-    // console.log('under corrects', response.results[0].correct_answer);    /*   */
-    // var incorrect_answers = response.results[0].incorrect_answers;    /*   */
-    // var answerGroup = [correct_answer , incorrect_answers];    /*   */
-
-
-
-
-    // var answerGroup = answerGroup.concat(incorrect_answers);
-    // console.log('answerGroup', answerGroup);
 
 
 });
